b5ff9a86ae7d66b658fcfe0c685cd0685c7bba70
morphling322 <kukuwkahnd@gmail.com>
2025-03-12 13:42:48 +0300

    рефакторинг с помощью линтера

--- src/main/java/ru/GeometryProgressionGame.java
+++ src/main/java/ru/GeometryProgressionGame.java
@@ -3,56 +3,65 @@ package ru;
 import java.util.Random;
 import java.util.Scanner;
 
-public class GeometryProgressionGame {
+/**
+ * Класс реализующий игру "Геометрическая последовательность".
+ * **/
+public final class GeometryProgressionGame {
 
-    public void run(Scanner scanner) {
-        Random random = new Random();
+  /**
+   * Запускает игру.
+   *
+   * @param scanner - считывает знания с консоли
+   **/
+  public void run(Scanner scanner) {
+    final Random random = new Random();
 
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        scanner.nextLine();
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-        System.out.println("What number is missing in the progression?\n");
+    System.out.println("Welcome to the Brain Games!");
+    System.out.print("May I have your name? ");
+    scanner.nextLine();
+    String name = scanner.nextLine();
+    System.out.println("Hello, " + name + "!");
+    System.out.println("What number is missing in the progression?\n");
 
-        for (int i = 0; i < 3; i++) {
-            int length = random.nextInt(6) + 5; // Длина прогрессии от 5 до 10
-            int start = random.nextInt(5) + 1;  // Начальное число от 1 до 5
-            int ratio = random.nextInt(5) + 2;  // Коэффициент от 2 до 6
-            int hiddenIndex = random.nextInt(length);
+    for (int i = 0; i < 3; i++) {
+      int length = random.nextInt(6) + 5;
+      int start = random.nextInt(5) + 1;
+      int ratio = random.nextInt(5) + 2;
+      int hiddenIndex = random.nextInt(length);
 
-            int[] progression = new int[length];
-            for (int j = 0; j < length; j++) {
-                progression[j] = start * (int) Math.pow(ratio, j);
-            }
+      int[] progression = new int[length];
 
-            int correctAnswer = progression[hiddenIndex];
-            progression[hiddenIndex] = -1;
+      for (int j = 0; j < length; j++) {
+        progression[j] = start * (int) Math.pow(ratio, j);
+      }
 
-            System.out.print("Question: ");
-            for (int num : progression) {
-                if (num == -1) {
-                    System.out.print(".. ");
-                } else {
-                    System.out.print(num + " ");
-                }
-            }
-            System.out.println();
+      final int correctAnswer = progression[hiddenIndex];
+      progression[hiddenIndex] = -1;
 
-            System.out.print("Your answer: ");
-            int answer = scanner.nextInt();
-
-            if (answer == correctAnswer) {
-                System.out.println("Correct!\n");
-            } else {
-                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
-                System.out.println("Let's try again, " + name + "!");
-                scanner.close();
-                return;
-            }
+      System.out.print("Question: ");
+      for (int num : progression) {
+        if (num == -1) {
+          System.out.print(".. ");
+        } else {
+          System.out.print(num + " ");
         }
+      }
+      System.out.println();
 
-        System.out.println("Congratulations, " + name + "!");
+      System.out.print("Your answer: ");
+      int answer = scanner.nextInt();
+
+      if (answer == correctAnswer) {
+        System.out.println("Correct!\n");
+      } else {
+        System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
+        System.out.println("Let's try again, " + name + "!");
         scanner.close();
+        return;
+      }
     }
+
+    System.out.println("Congratulations, "  + name + "!");
+    scanner.close();
+  }
 }
--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -2,22 +2,31 @@ package ru;
 
 import java.util.Scanner;
 
+/**
+ * Main класс.
+ **/
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Choose game: \n 1. Brain game \n 2. Geometry progression game");
-        Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
-        switch (choice) {
-            case 1:
-                BrainGameLCM lcm = new BrainGameLCM();
-                lcm.run(sc);
-                break;
-            case 2:
-                GeometryProgressionGame game = new GeometryProgressionGame();
-                game.run(sc);
-                break;
-            default:
-                System.out.println("Invalid choice");
-        }
+
+  /**
+   * Точка входа в программу.
+   *
+   * @param args - массив строк, которые передаются в приложение при его запуске (не используется)
+   **/
+  public static void main(String[] args) {
+    System.out.println("Choose game: \n 1. Brain game \n 2. Geometry progression game");
+    Scanner sc = new Scanner(System.in);
+    int choice = sc.nextInt();
+    switch (choice) {
+      case 1:
+        BrainGame lcm = new BrainGame();
+        lcm.run(sc);
+        break;
+      case 2:
+        GeometryProgressionGame game = new GeometryProgressionGame();
+        game.run(sc);
+        break;
+      default:
+        System.out.println("Invalid choice");
     }
+  }
 }
